e38fa03a46525da6ee0a0615d130e5ae7e4595a8
gdmec07150913 <408594990@qq.com>
2016-10-18 15:28:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pak_lup/menu/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.pak_lup.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mcount;
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mcount=0;
+        tv1=(TextView)findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater mi=getMenuInflater();
+       // mi.inflate(R.menu.mymenu,menu);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+       // mcount++;
+      //  menu.add(0,2+mcount,0,"菜单"+mcount);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle().toString(),Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.add(0,1,0,"点我了");
+        menu.add(0,1,0,"哈哈");
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+        return super.onContextItemSelected(item);
+    }
+}
